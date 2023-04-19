59a3aa67ade964c748cf924de436bb690108f95f
Bezawork <bezaworkalx@gmail.com>
2023-04-19 07:11:18 +0000

    Make directory and add install lainstinstalls

---
+++ 0x01-ES6_promise/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      files: ["**/*.test.js"],
+      env: { "jest/globals": true },
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+    },
+  ],
+  plugins: ["jest"],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
