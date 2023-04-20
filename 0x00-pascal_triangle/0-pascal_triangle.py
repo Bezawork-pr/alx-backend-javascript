e161fe4021b36f638fb006d767f9084e8021a30d
Bezawork <bezaworkalx@gmail.com>
2023-04-20 12:54:05 +0000

    reate a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    my_list = []
+    if n <= 0:
+        return my_list
+    for row in range(n):
+        listinlist = [1]
+        for column in range(row):
+            if column == row - 1:
+                my_append = 1
+            elif row % 2 != 0 and column == 0:
+                my_append = row * (column + 1)
+            elif row % 2 == 0 and column == 0:
+                my_append = row * (column + 1)
+            elif row % 2 == 0 and column == 1:
+                my_append = (row - 1) * 2
+            elif row % 2 != 0 and column == 1 and row > 3:
+                my_append = row * (column + 1)
+            elif row % 2 == 0 and (row - column) == 2:
+                my_append = row
+            else:
+                pass
+            listinlist.append(my_append)
+        my_list.append(listinlist)
+        del listinlist
+    return my_list
