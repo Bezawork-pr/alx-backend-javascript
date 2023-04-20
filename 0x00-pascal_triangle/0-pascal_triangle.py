c0564443182ff1b9610cf1255e31d17a2cc02c73
Bezawork <bezaworkalx@gmail.com>
2023-04-20 13:49:47 +0000

    Implement pascal triangle in python

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+from math import factorial
 def pascal_triangle(n):
     my_list = []
     if n <= 0:
         return my_list
     for row in range(n):
-        listinlist = [1]
+        new_list = [1]
         for column in range(row):
             if column == row - 1:
                 my_append = 1
@@ -20,7 +21,7 @@ def pascal_triangle(n):
                 my_append = row
             else:
                 pass
-            listinlist.append(my_append)
-        my_list.append(listinlist)
-        del listinlist
+            new_list.append(my_append)
+        my_list.append(new_list)
+        del new_list
     return my_list
