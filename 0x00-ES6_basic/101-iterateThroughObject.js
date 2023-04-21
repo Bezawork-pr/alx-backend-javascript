77ad6ea27fa93c7385ec898ce6ebc2bbbfaa7a3e
Bezawork <bezaworkalx@gmail.com>
2023-04-21 19:16:29 +0000

    Fix eslint error by changing == with ===

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
-	let text = ""
-	for (let index in reportWithIterator){
-		if (index == 0) {
-			text += reportWithIterator[index];
-		}
-		else {
-			text +=" | " + reportWithIterator[index];
-		}
-	}
-	return text;
-
+  let text = '';
+  for (const index in reportWithIterator) {
+    if (index === 0) {
+      text += reportWithIterator[index];
+    } else {
+      text += ` | ${reportWithIterator[index]}`;
+    }
+  }
+  return text;
 }
