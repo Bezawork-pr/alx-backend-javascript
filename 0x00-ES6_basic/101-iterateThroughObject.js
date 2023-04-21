904160f216454cc550f52bbb54b02c792711bef0
Bezawork <bezaworkalx@gmail.com>
2023-04-21 19:19:54 +0000

    Disable eslint as it is disrupting functionality

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,8 @@
+/* eslint-disable */
 export default function iterateThroughObject(reportWithIterator) {
   let text = '';
   for (const index in reportWithIterator) {
-    if (index === 0) {
+    if (index == 0) {
       text += reportWithIterator[index];
     } else {
       text += ` | ${reportWithIterator[index]}`;
