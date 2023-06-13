51e7e4d07a89b66fb058f1d7d80d20c74dc6984c
Bezawork <bezaworkalx@gmail.com>
2023-06-13 16:37:45 +0000

    Create required files

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+const fs = require('fs');
+
+module.exports = async function readDatabase(path) {
+  try {
+    const data = await fs.readFileSync(path, 'utf8').split('\n');
+    const myList = [];
+    let cs = 0;
+    let swe = 0;
+    const csList = [];
+    const sweList = [];
+    const firstNameList = [];
+    data.shift();
+    data.pop();
+    console.log(`Number of students: ${data.length}`);
+    for (let i = 0; i < data.length; i = i + 1) {
+      const details = data[i].split(',');
+      const myDict = {};
+      let firstname, lastname, age, field;
+      [
+        firstname, lastname, age, field,
+      ] = details;
+      myDict.firstname= firstname;
+      myDict.lastname = lastname;
+      myDict.age = age;
+      myDict.field = field;
+      firstNameList.push(firstname);
+
+      if (myDict.field === 'CS') {
+        cs += 1;
+        csList.push(myDict.firstname);
+      } else {
+        swe += 1;
+        sweList.push(myDict.firstname);
+      }
+      myList.push(myDict);
+    }
+    console.log(`Number of students in CS: ${cs}. List: ${csList.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe}. List: ${sweList.join(', ')}`);
+    return firstNameList;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
