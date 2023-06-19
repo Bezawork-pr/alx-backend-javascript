7144d3160beaf417ea4be4ed872ce9e05fca3e62
Bezawork <bezaworkalx@gmail.com>
2023-06-19 10:53:19 +0000

    Add beforeEach and afterEach hook

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -5,18 +5,21 @@ const expect = require('chai').expect;
 
 
 describe('Compare', function () {
-    it('call sendPaymentRequestToAPI with 100, and 20', function () {
-      const spyOnConsole = sinon.spy(console, 'log');
-      const spyOnFunction = sinon.spy(Utils, 'calculateNumber');
-      sendPaymentRequestToApi(100, 20);
-      expect(spyOnConsole.calledWith('The total is: 120')).to.be.true;
-      expect(spyOnFunction.calledOnce).to.be.true;
+    beforeEach(function callSpy() { 
+      spyOnConsole = sinon.spy(console, 'log');
+      spyOnFunction = sinon.spy(Utils, 'calculateNumber');
+    });
+
+    afterEach(function restore(){ 
       spyOnConsole.restore();
       spyOnFunction.restore();
     });
+    it('call sendPaymentRequestToAPI with 100, and 20', function () {
+      sendPaymentRequestToApi(100, 20);
+      expect(spyOnConsole.calledWith('The total is: 120')).to.be.true;
+      expect(spyOnFunction.calledOnce).to.be.true;
+    });
     it('call sendPaymentRequestToAPI with 10, and 20', function () {
-      const spyOnConsole = sinon.spy(console, 'log');
-      const spyOnFunction = sinon.spy(Utils, 'calculateNumber');
       sendPaymentRequestToApi(10, 20);
       expect(spyOnConsole.calledWith('The total is: 30')).to.be.true;
       expect(spyOnFunction.calledOnce).to.be.true;
