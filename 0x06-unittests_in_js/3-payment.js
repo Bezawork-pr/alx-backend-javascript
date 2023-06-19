e88665dd548083bbdd21fe90a562519fa7cab8e7
Bezawork <bezaworkalx@gmail.com>
2023-06-19 09:50:40 +0000

    Fix test fail

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,7 @@
 const Utils = require('./utils.js');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  console.log(typeof totalAmount);
   const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
   console.log(`The total is: ${result}`);
 }
-sendPaymentRequestToApi(100, 20);
+module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,14 @@
 const Utils = require('./utils.js');
 const sendPaymentRequestToApi = require('./3-payment.js');
 const sinon = require('sinon');
-const expect = require('expect');
+const expect = require('chai').expect;
 
 
 describe('Compare', function () {
     it('make sure the math used for sendPaymentRequestToApi(100, 20) is the same as Utils.calculateNumber', function () {
-      const spyOnFunction = Sinon.spy(Utils, 'calculateNumber');
-	
+      const spyOnFunction = sinon.spy(Utils, 'calculateNumber');
       const senRequest = sendPaymentRequestToApi(100, 20);
-      expect(spyOnFunction.calledOnceWithExactly('SUM', 100, 20)).to.equal(120);
-      expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(senRequest);
+      expect(spyOnFunction.calledOnce).to.be.true;
+      expect(spyOnFunction.calledWith('SUM', 100, 20)).to.be.true;
     });
 });
